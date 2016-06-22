d58d5b70d8334ca60ef8faaf1c26459d428be926
jcruz <jcruz@jcruz>
2016-06-22 12:20:06 -0300

    formatação do demoiselle-dao-generator

--- src/demoiselle-dao-generator/src/main/groovy/sk4j/generator/AppGenerator.groovy
+++ src/demoiselle-dao-generator/src/main/groovy/sk4j/generator/AppGenerator.groovy
@@ -15,18 +15,19 @@ class AppGenerator extends SkApp {
 	}
 
 	@Override
-	public void run() {
-		console.echo '*** demoiselle-dao-generator ***', ConsoleColor.YELLOW
+	public void beforeRun() {
+		if(!project.isMavenProject()) exit "O diretório não possui um projeto maven válido."
+		if(project.javaFiles.isEmpty()) exit "O projeto não possui nenhuma entidade."
+	}
 
+	@Override
+	public void run() {
+		console.echo '*** demoiselle-dao-generator - Gerador de DAO para o Framework Demoiselle ***\n', ConsoleColor.YELLOW
 		// Filtra no projeto todas as classes java com a annotation @Entity
 		def entities = project.javaFiles.findAll { it.hasAnnotation('Entity') }
-
 		// Exibe no console as opções de seleção das entidades
 		def selectedEntities = console.readopts(entities)
-
-		selectedEntities.each { EJavaFile javaFile ->
-			// Cria o arquivo *DAO.java com o template DaoGeneratorTemplate
-			file "${javaFile.path}../persistence/${javaFile.name}DAO.java", javaFile.merge(DaoGeneratorTemplate)
-		}
+		// Cria o arquivo *DAO.java com o template DaoGeneratorTemplate
+		selectedEntities.each { file "${it.path}../persistence/${it.name}DAO.java", it.merge(DaoGeneratorTemplate) }
 	}
 }
