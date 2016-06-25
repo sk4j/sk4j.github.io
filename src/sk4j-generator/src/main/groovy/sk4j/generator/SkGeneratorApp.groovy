212c24c4853a9db28440d92a12ccef5cb8365049
jcruz <jcruz@jcruz>
2016-06-25 16:21:20 -0300

    ajustes na geração de templates

--- src/sk4j-generator/src/main/groovy/sk4j/generator/SkGeneratorApp.groovy
+++ src/sk4j-generator/src/main/groovy/sk4j/generator/SkGeneratorApp.groovy
@@ -2,9 +2,6 @@ package sk4j.generator
 
 import sk4j.ConsoleColor
 import sk4j.SkApp
-import sk4j.generator.template.AppGeneratorTemplate
-import sk4j.generator.template.BuildGradleTemplate
-import sk4j.generator.template.GitignoreTemplate
 
 class SkGeneratorApp extends SkApp {
 
@@ -19,16 +16,20 @@ class SkGeneratorApp extends SkApp {
 
 		if(!context['projectName'].isEmpty()) {
 			console.echo ""
-			console.echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", ConsoleColor.CYAN
+			console.echo ">"*40, ConsoleColor.CYAN
 			mkdir "${context.sk4jHome}/src/${context.projectName}"
 			mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/groovy"
-			mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/groovy/sk4j/generator/template"
+			mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/groovy/sk4j/generator/"
+			mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/resources/templates"
 			mkdir "${context.sk4jHome}/src/${context.projectName}/bin"
 			mkdir "${context.sk4jHome}/src/${context.projectName}/build"
 
-			file "${context.sk4jHome}/src/${context.projectName}/build.gradle", BuildGradleTemplate
-			file "${context.sk4jHome}/src/${context.projectName}/.gitignore", GitignoreTemplate
-			file "${context.sk4jHome}/src/${context.projectName}/src/main/groovy/sk4j/generator/AppGenerator.groovy", AppGeneratorTemplate
+			file "${context.sk4jHome}/src/${context.projectName}/.gitignore", template('gitignore')
+			file "${context.sk4jHome}/src/${context.projectName}/build.gradle", template('build-gradle')
+			file "${context.sk4jHome}/src/${context.projectName}/src/main/groovy/sk4j/generator/AppGenerator.groovy", template('app-generator')
+			file "${context.sk4jHome}/src/${context.projectName}/src/main/resources/templates/readme.txt", template('readme-txt')
+			
+			console.echo "<"*40, ConsoleColor.CYAN
 		}
 	}
 }
