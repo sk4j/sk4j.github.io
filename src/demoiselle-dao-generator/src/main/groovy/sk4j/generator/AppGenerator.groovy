33193ce93754b7bf3550075e1c030bb8f561faec
jcruz <jcruz@jcruz>
2016-06-26 10:58:49 -0300

    ajustes no dao-gen

--- src/demoiselle-dao-generator/src/main/groovy/sk4j/generator/AppGenerator.groovy
+++ src/demoiselle-dao-generator/src/main/groovy/sk4j/generator/AppGenerator.groovy
@@ -15,7 +15,7 @@ class AppGenerator extends SkApp {
 		// Sai do gerador se o diretório de execução não for um projeto maven.
 		quit condition: !project.isMavenProject(), message: 'O diretório não possui um projeto maven válido.'
     	// Sair do gerador se o projeto não possuir nenhum arquivo java.		 
-		quit condition: project.javaFiles.isEmpty(), message: 'O projeto não possui nenhuma entidade.'
+		quit condition: project.javaFiles.isEmpty(), message: 'O projeto não possui nenhum arquivo java.'
 		// Filtra no projeto todas as classes java com a annotation @Entity
 		def entities = project.javaFiles.findAll { it.hasAnnotation('Entity') }
 		// Exibe no console as opções de seleção das entidades
