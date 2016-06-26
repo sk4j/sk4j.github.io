fbf4e8db796dc151efb7c56b3184e3a6456894a9
jcruz <jcruz@jcruz>
2016-06-26 10:51:46 -0300

    generator

--- src/sk4j-generator/src/main/groovy/sk4j/generator/SkGeneratorApp.groovy
+++ src/sk4j-generator/src/main/groovy/sk4j/generator/SkGeneratorApp.groovy
@@ -11,30 +11,28 @@ class SkGeneratorApp extends SkApp {
 
 	@Override
 	public void run() {
-		console.echo '*** SkGeneratorApp ***', ConsoleColor.YELLOW
 		context['projectName'] = console.readln('Digite o nome do projeto')
 
-		if(!context['projectName'].isEmpty()) {
-			console.echo ""
-			mkdir "${context.sk4jHome}/src/${context.projectName}"
-			mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/groovy"
-			mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/groovy/sk4j/generator/"
-			mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/resources/templates"
-			mkdir "${context.sk4jHome}/src/${context.projectName}/bin"
-			mkdir "${context.sk4jHome}/src/${context.projectName}/build"
+		quit condition: context['projectName'].isEmpty(), message: 'Nome de projeto inválido.'
 
-			file path: "${context.sk4jHome}/src/${context.projectName}", 
-				 name: ".gitignore" , 
-				 template: 'gitignore'
-			file path: "${context.sk4jHome}/src/${context.projectName}",
-				 name: "build.gradle" ,
-				 template: 'build-gradle'
-			file path: "${context.sk4jHome}/src/${context.projectName}/src/main/groovy/sk4j/generator", 
-				 name: "AppGenerator.groovy" , 
-				 template: 'app-generator'
-			file path: "${context.sk4jHome}/src/${context.projectName}/src/main/resources/templates", 
-				 name: "readme.txt" , 
-				 template: 'readme-txt'
-		}
+		mkdir "${context.sk4jHome}/src/${context.projectName}"
+		mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/groovy"
+		mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/groovy/sk4j/generator/"
+		mkdir "${context.sk4jHome}/src/${context.projectName}/src/main/resources/templates"
+		mkdir "${context.sk4jHome}/src/${context.projectName}/bin"
+		mkdir "${context.sk4jHome}/src/${context.projectName}/build"
+
+		file path: "${context.sk4jHome}/src/${context.projectName}",
+			 name: ".gitignore" ,
+			 template: 'gitignore'
+		file path: "${context.sk4jHome}/src/${context.projectName}",
+			 name: "build.gradle" ,
+			 template: 'build-gradle'
+		file path: "${context.sk4jHome}/src/${context.projectName}/src/main/groovy/sk4j/generator",
+			 name: "AppGenerator.groovy" ,
+			 template: 'app-generator'
+		file path: "${context.sk4jHome}/src/${context.projectName}/src/main/resources/templates",
+			 name: "readme.txt" ,
+			 template: 'readme-txt'
 	}
 }
