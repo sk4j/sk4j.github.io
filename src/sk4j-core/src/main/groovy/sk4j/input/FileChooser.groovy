680db6e612ffb730f78979136222b3ad527ac131
jcruz <jcruz@jcruz>
2016-06-30 12:18:18 -0300

    criação do filechooser

---
+++ src/sk4j-core/src/main/groovy/sk4j/input/FileChooser.groovy
@@ -0,0 +1,26 @@
+package sk4j.input
+
+import sk4j.ConsoleColor
+
+class FileChooser implements Choosable<FileChooser> {
+
+	private File file
+
+	public FileChooser(File file) {
+		super()
+		this.file = file
+	}
+
+	@Override
+	public int compareTo(FileChooser o) {
+		return file.name.compareTo(o.file.name)
+	}
+
+	@Override
+	public String getChoiseLabel() {
+		def greenColor = ConsoleColor.GREEN.value
+		def whiteColor = ConsoleColor.WHITE.value
+		def bold = ConsoleColor.BOLD.value
+		return "${bold}${this.file.name}${whiteColor} - ${greenColor}${file.absolutePath}${whiteColor}"
+	}
+}
