b5513084d4c87f16076a70a5049dd7696ac3d9ca
jcruz <jcruz@jcruz>
2016-07-04 18:18:16 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+SK4J_HOME="~/git/sk4j.github.io/"
+GIT_HOME="~/git/"
+
 echo "Procurando pelo git..."
 if [ -z $(which git) ]; then
-	echo "Git não encontrado. Tentando instalação via apt-get"
+	echo "Git não encontrado. Tente instalar via apt-get depois reinicie o procedimento."
 	echo "======================================================================================================"
-	sudo apt-get install git
+	exit 0
 fi
 
+if [ ! -d "$GIT_HOME" ]; then
+	mkdir -p "$GIT_HOME"
+fi
+
+
+if [ -d "$SK4J_HOME" ]; then
+	echo "O sk4j já está instalado!"
+	echo "======================================================================================================"
+	exit 0
+fi
+
+
+
 echo "Clonando diretório sk4j..."
+cd "$GIT_HOME"
 git clone "https://github.com/sk4j/sk4j.github.io.git"
 
 echo "Instalando o SK4J..."
