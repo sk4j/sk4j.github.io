8dc7fb104e9a8b9dc8e1ad01ef33222b4f0c3c7e
suporte <suporte@suporte>
2016-07-04 20:49:14 -0300

    autocomplete

--- conf/sk4j-autocomplete.sh
+++ conf/sk4j-autocomplete.sh
@@ -3,22 +3,31 @@
 SK4J_DIR="${HOME}/git/sk4j.github.io"
 __sk_autocomplete ()   #  By convention, the function name
 {                 #+ starts with an underscore.
-  local cur artifacts
+  local cur prev opts 
+  local artifacts
   # Pointer to current completion word.
   # By convention, it's named "cur" but this isn't strictly necessary.
 
   COMPREPLY=()   # Array variable storing the possible completions.
   cur=${COMP_WORDS[COMP_CWORD]}
-
-  case "$cur" in
-    -e)
-        for f in $(find "${SK4J_DIR}/artifact" -type f -name '*.jar' -exec basename {} \;); do
+  prev=${COMP_WORDS[COMP_CWORD-1]}	  
+  opts="-e -h -l"  
+ 	
+  case "$prev" in
+   
+   -e)
+        for f in $(find "${SK4J_DIR}/artifact" -type f -name '*.jar' -exec basename -s .jar {} \;); do
             artifacts="$f $artifacts"
         done
-        COMPREPLY=( $( compgen -W "$artifacts" -- $cur ) );;
+        COMPREPLY=( $( compgen -W "$artifacts" -- $cur ) )
+	return 0 
+	;;
     -*)
-        COMPREPLY=( $( compgen -W '-e -l -h' -- $cur ) );;
+       COMPREPLY=( $( compgen -W "$opts" -- $cur ) );;
+
   esac
+
+  #COMPREPLY=($(compgen -W "$opts" -- "$cur"))		
 
   return 0
 }
