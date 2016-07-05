557f4ec690ada5ab2939d5ae103de7a16b91ba3b
jcruz <jcruz@jcruz>
2016-07-05 14:07:22 -0300

    refactor

--- src/demoiselle-dao-generator/src/main/groovy/sk4j/App.groovy
+++ src/demoiselle-dao-generator/src/main/groovy/sk4j/App.groovy
@@ -8,25 +8,44 @@ import sk4j.model.EJavaFile
 
 class App extends SkApp {
 
+	def selectedEntities = []
+
 	static main(args) {
 		new App().start(args)
 	}
 
 	@Override
 	public void run() {
-		// Sai do gerador se o diretório de execução não for um projeto maven.
-		quit condition: !project.isMavenProject(), message: 'O diretório não possui um projeto maven válido.'
-		// Sair do gerador se o projeto não possuir nenhum arquivo java.
-		quit condition: project.javaFiles.isEmpty(), message: 'O projeto não possui nenhum arquivo java.'
-		// Filtra no projeto todas as classes java com a annotation @Entity
-		def entities = project.javaFiles.findAll { it.hasAnnotation('Entity') }
-		// Exibe no console as opções de seleção das entidades
-		def selectedEntities = console.readopts('Selecione a(s) entidade(s)',entities)
-		console.log "Entidades selecionadas: ${selectedEntities*.name}"
+		chekProject()
+		selectEntities()
+		createDaoFileForEntities()
+	}
+
+	/*
+	 * 
+	 */
+	private createDaoFileForEntities() {
 		// Cria o arquivo *DAO.java com o template 'dao.jtwig'
 		selectedEntities.each { EJavaFile jf ->
 			fs.createFile path:"${jf.path}../persistence", name:"${jf.name}DAO.java", template: 'dao', model: jf
 		}
+	}
 
+	private selectEntities() {
+		// Filtra no projeto todas as classes java com a annotation @Entity
+		def entities = project.javaFiles.findAll { it.hasAnnotation('Entity') }
+		// Exibe no console as opções de seleção das entidades
+		selectedEntities = console.readopts('Selecione a(s) entidade(s)',entities)
+		console.log "Entidades selecionadas: ${selectedEntities*.name}"
+	}
+
+	/*
+	 * 
+	 */
+	private chekProject() {
+		// Sai do gerador se o diretório de execução não for um projeto maven.
+		quit condition: !project.isMavenProject(), message: 'O diretório não possui um projeto maven válido.'
+		// Sair do gerador se o projeto não possuir nenhum arquivo java.
+		quit condition: project.javaFiles.isEmpty(), message: 'O projeto não possui nenhum arquivo java.'
 	}
 }
