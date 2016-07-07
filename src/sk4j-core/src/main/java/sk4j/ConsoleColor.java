367fb14ac8ad4cb0848de0a037dd208dd85628bd
jcruz <jcruz@jcruz>
2016-07-07 08:25:08 -0300

    migração para java8

--- src/sk4j-core/src/main/java/sk4j/ConsoleColor.java
+++ src/sk4j-core/src/main/java/sk4j/ConsoleColor.java
@@ -1,5 +1,10 @@
 package sk4j;
 
+/**
+ * 
+ * @author jcruz
+ *
+ */
 public enum ConsoleColor {
 	WHITE("\033[0m"), BLUE("\033[94m"), YELLOW("\033[33m"), MAGENTA("\033[95m"), CYAN("\033[96m"), RED("\033[91m"), GRAY("\033[90m"), GREEN("\033[32m"), BOLD("\033[1m");
 
