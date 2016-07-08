e56c9342990edb5df78540c2faed1ff4e42b41ee
jcruz <jcruz@jcruz>
2016-07-08 14:20:34 -0300

    ajuste na classe Skfs

--- src/sk4j-core/src/main/java/sk4j/core/Skfs.java
+++ src/sk4j-core/src/main/java/sk4j/core/Skfs.java
@@ -1,6 +1,10 @@
 package sk4j.core;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import sk4j.core.console.CColor;
@@ -29,11 +33,45 @@ public class Skfs {
 		SkConsole.log("Diretório existente: %s", dir.getAbsolutePath());
 	}
 
-	public static void createFile(Path filePath, String fileName, SkTemplate template) {
+	/**
+	 * 
+	 * @param filePath
+	 * @param fileName
+	 * @param template
+	 * @throws IOException
+	 */
+	public static void createFile(Path filePath, String fileName, SkTemplate template) throws IOException {
+		createFile(filePath, fileName, template.merge());
+	}
+
+	/**
+	 * 
+	 * @param filePath
+	 * @param fileName
+	 * @param content
+	 * @throws IOException
+	 */
+	public static void createFile(Path filePath, String fileName, String content) throws IOException {
 		File file = new File(String.format("%s/%s", filePath.toFile().getAbsolutePath(), fileName));
 		if (file.exists()) {
 			SkConsole.log("Arquivo existente: %s/%s", filePath.toFile().getAbsolutePath(), fileName);
+			return;
 		}
+		new Skfs().write(file.toPath(), content);
+	}
+
+	/**
+	 * 
+	 * @param path
+	 * @param content
+	 * @throws IOException
+	 */
+	private void write(Path path, String content) throws IOException {
+		BufferedWriter writer = Files.newBufferedWriter(path);
+		writer.write(content);
+		writer.flush();
+		writer.close();
+		SkConsole.log("%s Arquivo criado:   %s", CColor.cyan(">>>"), path.toFile().getAbsolutePath());
 	}
 
 }
