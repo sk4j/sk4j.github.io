f887f8ee2bbfd4e1334d7ef158d8d044f3cfbe98
jcruz <jcruz@jcruz>
2016-07-21 13:15:35 -0300

    novo analyzer

---
+++ src/entity-analyzer/src/main/java/sk4j/analyzer/EntityHasEqualsAnalyzer.java
@@ -0,0 +1,32 @@
+package sk4j.analyzer;
+
+import javax.inject.Inject;
+
+import sk4j.core.EntityAnalizer;
+import sk4j.core.EntityReport;
+import sk4j.core.model.EJavaFile;
+
+/**
+ * Verifica se a entidade implementa o hashCode.
+ * 
+ * @author jcruz
+ *
+ */
+public class EntityHasEqualsAnalyzer implements EntityAnalizer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EntityReport report;
+
+	@Override
+	public void analyze(EJavaFile javaFile) {
+		if (!javaFile.getJavaMethods().stream().anyMatch(method -> method.getName().equals("equals"))) {
+			report.addReportItem(javaFile.getName(), "A classe deve implementar equals.");
+		}
+	}
+
+}
