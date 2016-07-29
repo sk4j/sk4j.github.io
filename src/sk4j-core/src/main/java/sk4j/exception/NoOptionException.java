5c48bfeb9b00f9ebfb3d64a084f7ff8c017bb896
jcruz <jcruz@jcruz>
2016-07-29 15:55:29 -0300

    update no core

---
+++ src/sk4j-core/src/main/java/sk4j/exception/NoOptionException.java
@@ -0,0 +1,14 @@
+package sk4j.exception;
+
+public class NoOptionException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoOptionException(String message) {
+		super(message);
+	}
+
+}
