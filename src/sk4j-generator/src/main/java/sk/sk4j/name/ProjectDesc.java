82a6fc165a50916e19be434e41afe37127874d09
jcruz <jcruz@jcruz>
2016-08-16 17:22:11 -0300

    ajuste no core

--- src/sk4j-generator/src/main/java/sk/sk4j/name/ProjectDesc.java
+++ src/sk4j-generator/src/main/java/sk/sk4j/name/ProjectDesc.java
@@ -1,5 +1,7 @@
 package sk.sk4j.name;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import sk4j.input.Name;
 
 public class ProjectDesc extends Name {
@@ -8,5 +10,11 @@ public class ProjectDesc extends Name {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	@NotEmpty
+	public String getValue() {
+		return super.getValue();
+	}
 
 }
--- src/sk4j-generator/src/main/java/sk/sk4j/name/ProjectName.java
+++ src/sk4j-generator/src/main/java/sk/sk4j/name/ProjectName.java
@@ -13,6 +13,7 @@ public class ProjectName extends Name {
 
 	@Override
 	@NotBlank(message = "O nome do projeto não pode ser em branco.")
+	// @Pattern.List(value = { @Pattern(regexp = "[\\s]*", message = "O nome do projeto não pode conter espaços.") })
 	public String getValue() {
 		return super.getValue();
 	}
